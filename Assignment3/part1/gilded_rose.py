fbd1733fd8ffcd49c6adbeb6deceb62f089a3cee
Zongyi000 <zongyijiang98@gmail.com>
2023-10-01 01:34:36 -0700

    assignment3

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -5,7 +5,9 @@ class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
     def __init__(self, name, sell_in, quality):
         self.name = name
+        #the number of days we have to sell the item
         self.sell_in = sell_in
+        #how valuable the item is, [0,50]
         self.quality = quality
 
     def __repr__(self):
@@ -20,30 +22,26 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue  
+
+            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in > 10:
+                    item.quality += 1 
+                elif 6 <= item.sell_in <= 10:
+                    item.quality += 2 
+                elif 1 <= item.sell_in <= 5:
+                    item.quality += 3 
+                else:  # Concert has passed 
+                    item.quality = 0 
+                item.quality = min(item.quality, 50)  # Ensure quality does not exceed 50
+                item.sell_in -= 1
+
+            else:  # Other items
+                decrement = 1 
+                if 'Conjured' in item.name:
+                    decrement *= 2
+                if item.sell_in <= 0:
+                    decrement *= 2  # degrade twice as fast after sell-by date
+                item.quality = max(0, item.quality - decrement)
+                item.sell_in -= 1
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -5,11 +5,74 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # def test_foo(self):
+    #     items = [Item("foo", 0, 0)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     self.assertEquals("fixme", items[0].name)
+    def test_normal_item_before_sell_date(self):
+        items = [Item("foo", 5, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(9, items[0].quality)
+
+    def test_normal_item_after_sell_date(self):
+        items = [Item("foo", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
+    def test_Aged_Brie_before_sell_date(self):
+        items = [Item("Aged Brie", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(13, items[0].quality)
+
+    def test_Backstage_passes_more_than_10_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(14, items[0].sell_in)
+        self.assertEqual(11, items[0].quality)
+
+    def test_Backstage_passes_less_than_10_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 9, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(8, items[0].sell_in)
+        self.assertEqual(12, items[0].quality)
+
+    def test_Backstage_passes_less_than_5_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(3, items[0].sell_in)
+        self.assertEqual(13, items[0].quality)
+
+    def test_Backstage_passes_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
+
+    def test_Conjured_item_before_sell_date(self):
+        items = [Item("Conjured Mana Cake", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
+    def test_Conjured_item_after_sell_date(self):
+        items = [Item("Conjured Mana Cake", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(6, items[0].quality)
 
 
 if __name__ == '__main__':
